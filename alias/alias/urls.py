b65b0452a4d7ffab7186d1e98d65a3f43161ad28
Massimiliano Pippi <massi@datadoghq.com>
2016-09-17 11:17:25 +0200

    admin site, first draft

--- alias/alias/urls.py
+++ alias/alias/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
+
+from school.admin import admin_site
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin_site.urls),
 ]
--- alias/school/admin.py
+++ alias/school/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 
-# Register your models here.
+from .models import *
+
+
+class SchoolAdminSite(AdminSite):
+    site_header = 'Monty Python administration'
+admin_site = SchoolAdminSite(name='myadmin')
+
+admin_site.register(Person)
+admin_site.register(Partner)
+admin_site.register(Lodge)
+admin_site.register(Student)
+admin_site.register(CourseLevel)
+admin_site.register(Session)
+admin_site.register(CostItem)
+admin_site.register(Course)
+admin_site.register(Staying)
